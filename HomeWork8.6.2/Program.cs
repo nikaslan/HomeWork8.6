16ed2acab40abefd33982d07445a0d03dd8c6bce
Nikita <email@domain.net>
2022-01-30 00:49:01 +0300

    Initial commit. Task 1 and 2

---
+++ HomeWork8.6.2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork8._6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Dictionary<string, string> phoneBook = new Dictionary<string, string>();            
+
+            while (true) // цикл по добавлению записей в записную книжку
+            {
+                Console.Clear();
+                phoneBook = AddNewEntry(phoneBook);
+                
+                Console.WriteLine("Для завершения добавления записей нажмите Enter.");
+                Console.WriteLine("Для добавления еще одной записи, нажмите любую другую кнопку.");
+                var key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Enter) break;
+                
+            }
+
+            while (true) // цикл по выводу значений по запросу
+            {
+                Console.WriteLine("Для продолжения нажмите Enter. Для выхода нажмите Esc.");
+                var key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Escape) break;
+                else if (key.Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    FindContact(phoneBook);
+                }
+            }
+
+
+        }
+        /// <summary>
+        /// Добавление новой записи в словарь
+        /// </summary>
+        /// <param name="phoneBook">Принимаем словарь в текущем состоянии</param>
+        /// <returns>Возвращаем обновленный словарь</returns>
+        static Dictionary<string, string> AddNewEntry(Dictionary<string, string> phoneBook)
+        {
+            string[] newEntry = new string[2];
+
+            Console.WriteLine("Введите номер телефона");
+            newEntry[0] = Console.ReadLine();
+            Console.WriteLine("Введите номер Фамилию владельца телефона");
+            newEntry[1] = Console.ReadLine();
+            if (phoneBook.ContainsKey(newEntry[0]))
+            {
+                Console.WriteLine("Такой номер телефона уже зарегистрирован");
+            }
+            else
+            {
+                Console.WriteLine($"В записную книжку добавлена запись: {newEntry[0]} - {newEntry[1]}.");
+                phoneBook.Add(newEntry[0], newEntry[1]);
+            }
+
+            return phoneBook;
+        }
+        /// <summary>
+        /// Нахождение имени контакта по введенному номеру телефона
+        /// </summary>
+        /// <param name="phoneBook">передаем телефонную книгу в ее текущем состоянии</param>
+        static void FindContact(Dictionary<string, string> phoneBook)
+        {
+            Console.WriteLine("Введите номер телефона, для отображения соответствующего контакта.");
+            string phoneNum = Console.ReadLine();
+
+            if (phoneBook.TryGetValue(phoneNum, out string name))
+            {
+                Console.WriteLine($"Владелец телефона {phoneNum} - {name}");
+                return;
+            }
+            else Console.WriteLine("Такого номера нет в записной книжке");
+        }
+
+    }
+}
---
+++ HomeWork8.6/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork8._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            List<int> randomNums = FillInList(); // инициализируем список случайных чисел
+            Console.WriteLine("Следующий список чисел был сгенерирован:");
+            PrintList(randomNums);
+            randomNums = ModifyList(randomNums); // модифицируем список, удаляя значения, согласно условиям
+            Console.WriteLine("После удаления значений меньше 25, но больше 50, получился следующий список:");
+            PrintList(randomNums);
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// Создание списка и заполнение его случайными числами от 0 до 100
+        /// </summary>
+        /// <returns>возвращаем сгенерированный список</returns>
+        static List<int> FillInList ()
+        {
+            Random rand = new Random();
+            
+            List<int> tempList = new List<int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                int j = rand.Next(100);
+                tempList.Add(j);
+            }
+
+            return tempList;
+        }
+        /// <summary>
+        /// выводим список в консоль
+        /// </summary>
+        /// <param name="tempList">передаем список для вывода</param>
+        static void PrintList(List<int> tempList)
+        {
+            Console.WriteLine("\n");
+            foreach (var i in tempList)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine("\n");
+        }
+
+        /// <summary>
+        /// Изменяем список, удаляя значения, которые находятся между 25 и 50
+        /// </summary>
+        /// <param name="tempList">передаем начальный список</param>
+        /// <returns>возвращаем модифицированный список</returns>
+        static List<int> ModifyList(List<int> tempList)
+        {
+            for(int i = 0; i< tempList.Count; i++)
+            {
+                if (tempList[i] > 25 && tempList[i] < 50)
+                {
+                    tempList.RemoveAt(i);
+                    i--; // чтобы на следующей итерации цикла мы снова прочитали i-ый элемент, т.к. он изменился после удаления предыдущего значения
+                }
+            }
+            return tempList;
+        }
+    }
+}
